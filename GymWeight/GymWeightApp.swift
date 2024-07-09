a8ba08c7933f5029eb20bd26bb29d0bfb6b3bb55
Ramon Xavier <ramon1234rxm@gmail.com>
2024-07-08 21:01:46 -0300

    Initial commit

---
+++ GymWeight/GymWeightApp.swift
@@ -0,0 +1,32 @@
+//
+//  GymWeightApp.swift
+//  GymWeight
+//
+//  Created by Ramon Xavier on 08/07/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GymWeightApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
